a1ca164a014f728ddf8410690c245f81336cf4d8
Abdulrahman <eng.abdulrahmanbahyan@gmail.com>
2025-10-19 16:14:35 +0300

    create grpup dialog

---
+++ public/js/groups/add-group.js
@@ -0,0 +1,21 @@
+import { animate, stagger } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm"
+
+const addGroupBtn = document.getElementById("add-group-btn");
+
+if (addGroupBtn) {
+    addGroupBtn.addEventListener("click", () => {
+        animate("#add-group-modal", { opacity: 1, y: [100, 0] }, { delay: stagger(0.05) })
+    });
+}
+
+const descriptionControl = document.querySelector("#add-group-modal #description-control");
+const descriptionLength = document.querySelector("#add-group-modal #description-length");
+
+if (descriptionControl && descriptionLength) {
+    descriptionControl.addEventListener("input", (e) => {
+        const value = e.target.value;
+        const length = value.length;
+
+        descriptionLength.textContent = `${length} / 100`;
+    });
+}
